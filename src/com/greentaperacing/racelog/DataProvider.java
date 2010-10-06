a96164e3e87eb7ecee24d31c10923916a8696f6a
Patrick O'Leary <patrick.oleary@gmail.com>
2010-10-05 22:53:55 -0500

    Skeleton for a DataProvider.

---
+++ src/com/greentaperacing/racelog/DataProvider.java
@@ -0,0 +1,15 @@
+package com.greentaperacing.racelog;
+
+/**
+ * An interface describing a data backend:
+ * Native sensors
+ * OBD II
+ * MegaSquirt
+ * ???
+ * 
+ * Should provide information about the available types of data, and provide a generic way for the
+ * data-handling layer to access the sensor data
+ */
+public interface DataProvider {
+
+}
